94d3a92844fc3b389f449b9096d9510337953554
loctc <loctcu@gmail.com>
2023-12-22 21:26:02 +0700

    completed

---
+++ src/components/NameCard/index.js
@@ -0,0 +1,24 @@
+// =========================Truyền theo kiểu props ============================
+import './style.css';
+
+const NameCard = (props) => {
+    const { name, age, gender } = props;
+
+    // Xác định màu nền dựa trên gender
+    let backgroundColor;
+    if (gender === "male") {
+        backgroundColor = "green";
+    } else {
+        backgroundColor = "red";
+    }
+
+    return (
+        <div className="card" style={{ backgroundColor }}>
+            <div className="name">Name: {name}</div>
+            <div className="age">Age: {age}</div>
+            <div className="gender">Gender: {gender}</div>
+        </div>
+    );
+};
+
+export default NameCard;
