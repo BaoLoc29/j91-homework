9e863192c14d627d6ef691ae98f3e4aa49af1b31
loctc <loctcu@gmail.com>
2024-01-09 20:44:02 +0700

    completed hw-7

--- src/components/TodoItem/index.js
+++ src/components/TodoItem/index.js
@@ -1,13 +1,44 @@
+import { useState } from 'react';
+import './style.css'; // Import file CSS
 
-// const TodoItem = ({ id, todo, isCompleted }) => {
-//     return (
+const TodoItem = ({ todo, onComplete, onDelete }) => {
+  const [isChecked, setIsChecked] = useState(todo.isCompleted);
 
-//         <div key={todoItem.id}>
-//             <p>Id: {todoItem.id}</p>
-//             <p>Todo: {todoItem.todo}</p>
-//             <p>Completed: {todoItem.isCompleted = isCompleted ? "yes" : "no"}</p>
-//         </div>
-//     )
+  const handleCheckboxChange = () => {
+    setIsChecked(!isChecked);
+    onComplete(todo.id);
+  };
 
-// }
-// export default TodoItem;
+  const handleDelete = () => {
+    onDelete(todo.id);
+  };
+
+  return (
+    <div className="task">
+      <div className='task-item'>
+        <div className='it-task'>
+          <div className='item-left'>
+            <div className='check-left'>
+              <input type="checkbox" checked={isChecked} onChange={handleCheckboxChange} />
+            </div>
+            <div className='name-todo'>
+              <span className={isChecked ? 'completed' : ''}>
+                {todo.todo}
+              </span>
+            </div>
+          </div>
+          <div className='item-right'>
+            <span className='num-todo'>
+              <span className='num-count'>0/1</span>
+            </span>
+            <div className='btn-deleted'>
+              <button onClick={handleDelete}>X</button>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default TodoItem;
